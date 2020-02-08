67902196065d0d3daf639d56cdede84f38b4b9ad
AlreadyBored <shilaums@gmail.com>
2020-02-08 20:40:08 +0300

    feat: obfuscate vigenere-cipher tests

--- test/vigenere-cipher.test.js
+++ test/vigenere-cipher.test.js
@@ -5,40 +5,7 @@ Object.freeze(assert);
 
 const VigenereCipheringMachine = require('../src/vigenere-cipher.js');
 
-
-const createTestString = (length) => {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^%';
-
-    let res = '';
-
-    const getRandomChar = () => {
-        const randomIndex = Math.floor(Math.random() * alphabet.length);
-        return alphabet[randomIndex];
-    };
-
-    for (let i = 0; i < length; i += 1) {
-        res += getRandomChar();
-    }
-
-    return res;
-};
-
-const createTestKey = (length) => {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    let res = '';
-
-    const getRandomChar = () => {
-        const randomIndex = Math.floor(Math.random() * alphabet.length);
-        return alphabet[randomIndex];
-    };
-
-    for (let i = 0; i < length; i += 1) {
-        res += getRandomChar();
-    }
-
-    return res;
-};
+const _0x3a46=['random','length','ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^%','ABCDEFGHIJKLMNOPQRSTUVWXYZ','floor'];(function(_0x17e833,_0x3fc88c){const _0x5d898f=function(_0x50eaa9){while(--_0x50eaa9){_0x17e833['push'](_0x17e833['shift']());}};_0x5d898f(++_0x3fc88c);}(_0x3a46,0xa9));const _0x42a8=function(_0x17e833,_0x3fc88c){_0x17e833=_0x17e833-0x0;let _0x5d898f=_0x3a46[_0x17e833];return _0x5d898f;};const createTestString=_0x46016b=>{const _0x1a8e89=_0x42a8('0x3');let _0x450411='';const _0x2e121f=()=>{const _0x4a06fa=Math['floor'](Math['random']()*_0x1a8e89[_0x42a8('0x2')]);return _0x1a8e89[_0x4a06fa];};for(let _0x1ebd06=0x0;_0x1ebd06<_0x46016b;_0x1ebd06+=0x1){_0x450411+=_0x2e121f();}return _0x450411;};const createTestKey=_0x349a0c=>{const _0x22c5a7=_0x42a8('0x4');let _0x2079de='';const _0x452ad9=()=>{const _0x13d2ac=Math[_0x42a8('0x0')](Math[_0x42a8('0x1')]()*_0x22c5a7['length']);return _0x22c5a7[_0x13d2ac];};for(let _0x4e34f4=0x0;_0x4e34f4<_0x349a0c;_0x4e34f4+=0x1){_0x2079de+=_0x452ad9();}return _0x2079de;};
 
 describe('Vigenere cipher', () => {
     // Presence requirement
