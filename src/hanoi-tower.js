1c5f9d936f0bef9e4c250c0e34b59fb2c3e1be45
Atamananet <storozhenko.vladimir.vasilhevich@gmail.com>
2020-02-19 14:00:50 +0300

    feat: complete hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,6 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    return {
+        turns : 2**disksNumber - 1,
+        seconds : this.turns / (turnsSpeed / 3600),
+    }
 }
