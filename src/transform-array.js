956c4a197b05cb498cc01c220102da773546ad00
Andrey <andrey.sviontek@gmail.com>
2020-02-19 21:21:02 +0500

    feat: complete array transform task

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,19 +2,24 @@ module.exports = function transform(arr) {
     if (arr.length === 0) {
         return arr;
     }
-    if (arr.isArray()){
+    if (!Array.isArray(arr)){
         throw Error;
     }
-    for(i = 0; i < arr.length; i++) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
         if (arr[i] === '--discard-next') {
-            splice(arr[i], arr[i + 2]);
-        } else if (arr[i] === '--discard-prev') {
-            splice(arr[i-1], arr[i + 1]);
+                i++;
+        } else if (arr[i] === '--discard-prev') {            
+            newArr.pop();
         } else if (arr[i] === '--double-next') {
-            arr[i] = arr[i + 1];
+            if (i < arr.length - 1) {
+                newArr.push(arr[i + 1]);
+            }
         } else if (arr[i] === '--double-prev') {
-            arr[i] = arr[i - 1];            
-        }        
+            if (i > 0) {
+                newArr.push(arr[i - 1]);
+            }            
+        } else newArr.push(arr[i]);
     }
-    return arr;
+    return newArr;
 };
