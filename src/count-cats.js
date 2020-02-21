0aff7d47d47b1c7f244c2efe61beb67d32cfb2d7
unknown <bashkin2013@mail.ru>
2020-02-21 10:07:28 +0300

    feat: add function for countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+  let i = 0
+  array.map(el => {
+    el.map(e => {
+      if (e === '^^') i++;
+    })
+  })
+  return i;
 };
