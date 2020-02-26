2186d1ad275193aa72619f7db6ac13cde94396d4
Andrey <andrey.sviontek@gmail.com>
2020-02-26 21:08:32 +0500

    solution: easy task but unobvious test for NaN

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined){
+    return'Unable to determine the time of year!';
+  }
+  if (isNaN(date)) {
+    throw Error;
+  }
+
+  let month = date.getMonth();  
+  if(month >= 0 && month <= 1 || month === 11) {
+    return 'winter';
+  } else if (month >= 2 && month <= 4) {
+    return 'spring';
+  } else if (month >= 5 && month <= 7) {
+    return 'summer';
+  } else if(month >= 8 && month <= 10) {
+    return 'autumn';
+  }
 };
