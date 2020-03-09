1cccae884659c667663d9c9b92fce97a6f0d3481
Andrey <andrey.sviontek@gmail.com>
2020-03-09 10:51:15 +0500

    solution: one more is done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result ="";
+    let add="";
+    if (options.addition === undefined) {
+        options.addition = ''}
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|'}
+    if (options.separator===undefined) {
+        options.separator = '+'}
+    for (let i = 0; i < options.additionRepeatTimes - 1; i++){
+        add += options.addition + options.additionSeparator;
+    }
+    add += options.addition;
+    str = str + add;
+    for (let i = 0; i < options.repeatTimes - 1 ; i++){
+        result += str + options.separator;
+    }
+    result += str;
+    return result;
 };
   
