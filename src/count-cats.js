f06dc01bfc7f4f6f9720f1d495a8d75240948549
Hotja <dudechka@mail.ru>
2020-03-12 11:58:06 +0300

    count-cats:finished

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+ /* throw 'Not implemented';*/
+  if (!arguments.length) return [];
+
+  let cats = 0;
+  let arr = [];
+  matrix.forEach((x, i) => {Array.prototype.push.apply(arr, x)});
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] == '^^') cats += 1;
+  }
+  return cats;
+}
