031901cb6090cbbf514ea63bb6cc1f98c649ad54
Hotja <dudechka@mail.ru>
2020-03-12 17:01:23 +0300

    what-season:work

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,12 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  let str = date.toString();
+  let arr = str.split(' ');
+
+
+
+  /*if ( arr[1] == 'Dec' || arr[1] == 'Feb' || arr[1] == 'Jan') return 'winter'
+  if ( arr[1] == 2 || arr[1] == 3 || arr[1] == 4) return 'spring'
+  if ( arr[1] == 'Jun' || arr[1] == 'Jul' || arr[1] == 7) return 'summer'
+  if ( arr[1] == 8 || arr[1] == 9 || arr[1] == 10) return 'autumn'*/
 };
