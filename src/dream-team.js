74a3a19d5642c83bf8fe302e0e4569a4a9cef209
Hotja <dudechka@mail.ru>
2020-03-12 16:01:42 +0300

    dream-team:finished

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+ /* if (!Array.isArray(members)) return false
+  let TName = [];
+  for ( let i = 0; i < members.length; i++) {
+    let x = members[i].trim();
+    let y = x.substr(0,1);
+    TName.push(y.toUpperCase());
+  }
+  TName.sort()
+  let z = TName.join('');
+  return z;
+*/
+
+if (!Array.isArray(members)) return false
+return members.filter(m => typeof m == 'string')
+  .map(m => m.trim()[0].toUpperCase()).sort().join('')
+
+}
