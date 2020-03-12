9e2083040a8e1e456eebc65eb1e3f2b195a16eba
unknown <dudechka@mail.ru>
2020-03-12 21:31:05 +0300

    what-season:finished

--- src/what-season.js
+++ src/what-season.js
@@ -1,12 +1,13 @@
 module.exports = function getSeason(date) {
 
-  let str = date.toString();
-  let arr = str.split(' ');
+  if (date === undefined) return 'Unable to determine the time of year!'
+  
+  if( !date.getMilliseconds()) {throw Error}
+  let x = date.getMonth();
 
-
-
-  /*if ( arr[1] == 'Dec' || arr[1] == 'Feb' || arr[1] == 'Jan') return 'winter'
-  if ( arr[1] == 2 || arr[1] == 3 || arr[1] == 4) return 'spring'
-  if ( arr[1] == 'Jun' || arr[1] == 'Jul' || arr[1] == 7) return 'summer'
-  if ( arr[1] == 8 || arr[1] == 9 || arr[1] == 10) return 'autumn'*/
+  if ( x == 11 || x == 1 || x == 0) return 'winter'
+  if ( x == 2 || x == 3 || x == 4) return 'spring'
+  if ( x == 5 || x == 6 || x == 7) return 'summer'
+  if ( x == 8 || x == 9 || x == 10) return 'autumn'
+  
 };
