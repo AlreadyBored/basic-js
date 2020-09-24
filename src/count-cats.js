a9727e541972305f7c75b9acbb655e09ac020ed8
RomanShajnoga <shajnogar@gmail.com>
2020-09-24 20:30:57 +0300

    feat: count-cat finish

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+//  ? throw new CustomError('Not implemented');
+  return backyard.flat().filter(element => element === '^^').length;
 };
