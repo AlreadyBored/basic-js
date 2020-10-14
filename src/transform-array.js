5a7364adeed48b5ec4dbdb56052568e4757fd258
Andrey Pleskunou <apleskunov@bk.ru>
2020-10-15 02:31:27 +0300

    feat: typical array bla bla bla

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,6 +4,10 @@ module.exports = function transform(arr) {
         Array.isArray(arr);
         var newArr;
         newArr = arr.map((element, index) => {
+            if (arr[index] === '--double-prev') {
+                arr.splice(index, 1);
+
+            }
             if (arr[index + 1] === '--double-prev') {
                 arr[index + 1] = arr[index];
 
@@ -11,7 +15,13 @@ module.exports = function transform(arr) {
             if (arr[index + 1] === '--double-next') {
                 arr[index + 1] = arr[index + 2];
             }
+            if (arr[index] === '--discard-prev') {
+                arr.splice(index, 1);
+
+
+            }
             if (arr[index + 1] === '--discard-prev') {
+
                 arr[index + 1] = arr[index];
                 arr.splice(index, 2);
 
@@ -26,9 +36,8 @@ module.exports = function transform(arr) {
 
 
         });
+        console.log(arr);
         return arr;
 
     }
-    /* transform([1, 2, '--double-prev', 3, 4, '--discard-next', 5,
-        6, '--discard-prev', 7, 8, '--double-next', 9, 10
-    ]); */
+    /* transform(['--double-prev', 1, 2, 3]); */
