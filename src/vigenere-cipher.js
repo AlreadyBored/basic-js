8738575e56c359446070f6537be0b9b13096e01f
alex47spb <heap.styx@mail.ru>
2020-10-14 14:54:09 +0300

    feat: cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,139 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+
+  constructor( direct ){
+
+    if ( arguments.length === 0 ) this.direct = true;
+    else this.direct = direct;
+//     принимает true (или ничего), чтобы создать прямую машину
+
+// Прямая машина просто шифрует и дешифрует строку, переданную в нее
+
+// false, чтобы создать обратную машину
+// обратная машина возвращает 
+// перевернутую задом наперед строку после шифрования и дешифрования.
+
+  }
+
+  encrypt(message, key) {
+    const characters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    const N = characters.length;
+
+    const input = message.toUpperCase();
+    const maxIndexInput = input.length - 1;
+
+    const keyword = key.toUpperCase();
+    const maxIndexKeyword = keyword.length - 1;
+
+    let result = "";
+    let indexKeyword = 0;
+
+    for (let indexInput=0; indexInput <= maxIndexInput; indexInput++) {
+      let fromInput = characters.indexOf(input[indexInput]);
+
+      if (fromInput === -1) result += input[indexInput];
+      else {
+        let c = (fromInput + characters.indexOf(keyword[indexKeyword])) % N;
+
+        result += characters[c];
+
+        if (indexKeyword === maxIndexKeyword) indexKeyword = 0;
+        else indexKeyword += 1;
+      }
+    }
+//    console.log( 'encrypt: result= ' + result );
+
+    if( this.direct ) return result;
+    else return result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    const characters = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    const N = characters.length;
+
+    const input = message.toUpperCase();
+    const maxIndexInput = input.length - 1;
+
+    const keyword = key.toUpperCase();
+    const maxIndexKeyword = keyword.length - 1;
+
+    let result = "";
+    let indexKeyword = 0;
+
+    for (let indexInput=0; indexInput <= maxIndexInput; indexInput++) {
+      let fromInput = characters.indexOf(input[indexInput]);
+
+      if (fromInput === -1) result += input[indexInput];
+      else {
+        let c = (fromInput + N - characters.indexOf(keyword[indexKeyword])) % N;
+
+        result += characters[c];
+
+        if (indexKeyword === maxIndexKeyword) indexKeyword = 0;
+        else indexKeyword += 1;
+      }
+    }
+//    console.log( 'decrypt: result= ' + result );
+
+    if( this.direct ) return result;
+    else return result.split('').reverse().join('');
+
   }
 }
 
