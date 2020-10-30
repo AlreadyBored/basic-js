455b62a6a8031075f6f33673055607fd0bb3654a
Elena Ivanova <elenaxivanova@gmail.com>
2020-10-31 02:01:46 +0300

    feat: add 'Dream team'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = '';
+  if (Array.isArray(members) === true) {
+
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        for (let j = 0; j < members[i].length; j++) {
+          if (members[i].charAt(j) === ' ') {
+            continue;
+          } else {
+            result += members[i].charAt(j);
+            result = result.toUpperCase().split('').sort().join('').toString();
+            break;
+          }
+        }
+      } 
+    }
+  } 
+  if (result.length === 0) {
+    result = false;
+  }
+  return result;
 };
+
+
+
