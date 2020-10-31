2f605d5abe179523589fc82396b60288e84710ab
Tanya <webtormashki@gmail.com>
2020-10-31 15:58:48 +0300

    feat: add countCats algorithm

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  var a = 0;
+  for (var i = 0; i < backyard.length; i++) {
+    for (var j = 0; j < backyard[i].length; j++) {
+      if (backyard[i][j] === '^^') {
+        a += 1;
+      }
+    }
+  }
+  return a;
 };
