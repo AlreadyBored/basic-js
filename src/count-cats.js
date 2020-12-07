92c8d8c703f08ba843888c525746d980735dc2f8
ra-js-epam-training <ruslan_afanasiev@epam.com>
2020-12-07 20:39:52 +0000

    [TASK-01] Added a new func to count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(cats) {
+  let total = 0;
+  if (!Array.isArray(cats) || cats.length == 0) {
+    return total;
+  }
+
+  for (const element of cats) {
+    if (!Array.isArray(element) || element.length == 0) {
+      break;
+    }
+    total += element.reduce(function(sum, current) {
+      if (current === '^^') {
+        return sum += 1;
+      }
+      return sum;
+    }, 0);
+  }
+  return total;
 };
