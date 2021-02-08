653987cf156685809dbf65c9289dc853cb3cb30f
front42 <altaifront@gmail.com>
2021-02-08 13:38:02 +0300

    feat: add carbon-dating solution and refactor count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
+const NAT_LOG_2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string' && sampleActivity > 0 && sampleActivity < MODERN_ACTIVITY) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (NAT_LOG_2 / HALF_LIFE_PERIOD));
+  }
+  return false;
+}
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  matrix = matrix.join().split(',')
+  matrix = matrix.join().split(',');
   let count = 0;
   for (let i = 0; i < matrix.length; i++) {
     if (matrix[i] === "^^") {
