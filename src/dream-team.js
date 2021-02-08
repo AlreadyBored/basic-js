d208e7e1d3d334a6f8093372e9a3f59817dc0688
front42 <altaifront@gmail.com>
2021-02-08 23:01:00 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (members == null || members == undefined) {
+    return false;
+  }
+
+  let dreamNames = []
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      dreamNames.push(members[i].trim().toUpperCase());
+    }
+  }
+
+  let firstLetters = [];
+
+  for (let i = 0; i < dreamNames.length; i++) {
+    firstLetters.push(dreamNames[i][0]);
+  }
+
+  return firstLetters.sort().join('');
+  }
