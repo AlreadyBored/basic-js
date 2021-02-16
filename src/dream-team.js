62a837d47066f7a78fbd8da45ba80ec57f3bd5ac
Metamorg <egoronokasd@gmail.com>
2021-02-16 20:54:25 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+let array = [];
+if(!Array.isArray(members) || members.length===0){
+  return false;
+}
+for(let i=0; i < members.length; i++){
+    if (typeof members[i]==="string"){
+      array.push(members[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  return  String(array.sort().join(''));
 };
