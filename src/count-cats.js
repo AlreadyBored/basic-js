1c1c8f97ca639c4ee772c6a507a360362b15c499
Uladzislau Bolatau <uladzislau_bolatau@epam.com>
2021-02-24 11:22:54 +0300

    Count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  return backyard.flat().reduce((counter, element) => {
+    return counter += '^^' === element;
+  }, 0);
 };
