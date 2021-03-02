13d6a143c0e32359299fdd2a8d672352e5e3cedb
Uladzislau Bolatau <uladzislau_bolatau@epam.com>
2021-03-02 20:19:25 +0300

    Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let _options = Object.assign({
+    repeatTimes: 1,
+    separator: "+",
+    addition: "",
+    additionRepeatTimes: 1,
+    additionSeparator: "|"
+  }, options);
+  let separator = (_options.addition + _options.additionSeparator).repeat(_options.additionRepeatTimes - 1) + _options.addition;  
+
+  return (str + separator + _options.separator).repeat(_options.repeatTimes - 1) + str + separator;
 };
-  
