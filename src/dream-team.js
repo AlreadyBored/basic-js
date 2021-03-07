65955825ca0e27b8cb185e05185c58c4327c1d7f
furse2000 <furse2000@yandex.ru>
2021-03-08 01:17:11 +0300

    fix:fixed 2-nd func

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(memb) {
+ let temp='';
+ 
+  for(let i=0;i<memb.length;i++)
+  {
+    if((typeof memb[i] === "string" || memb[i] instanceof String)==0)
+    {
+      memb.splice(i,1);
+    }
+    for(let j=0;j<memb[i].length;j++)
+    {
+      if(memb[i][j]==' ')
+      { temp=memb[i];
+        temp.splice(j,1);
+        memb[i]=temp;
+      }
+    }
+    memb[i]=memb[i][0];
+    
+  }
+  /*for(let i=0;i<memb.length;i++)
+  {
+    for(let j=0;j<memb[i].length;j++)
+    {if(j=0)
+      memb[i][j]=memb[i][j];
+      
+    }
+    break;
+  }
+*/
+  if(memb.sort())
+  {
+ return memb.join('');
+ //temp=memb.String; 
+  //return temp.toUpperCase();
+  }
+  else return false
+  
 };
+
