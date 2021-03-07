f7c6e7cbc64add03929fce1c70025db78b7742be
Roman Yarovoi <roman.and.yarovoi@gmail.com>
2021-03-07 15:57:10 +0200

    feat: implement simple chain object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value='') {
+    this.chain.push(value)
+    return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (isNaN(position)) {
+      this.chain.length = 0
+      throw new Error(`${position} is not a number`)
+    }
+    this.chain.splice(position-1, 1)
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    try {
+      return this.chain.map(item => `( ${item} )`).join('~~');
+    } finally {
+      this.chain.length = 0
+    }
   }
 };
 
