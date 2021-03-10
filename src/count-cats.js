b62c1ae4cda0f2c559b1d0fc1cae0ded7bab7aa0
tokanito <78441278+tokanito@users.noreply.github.com>
2021-03-10 16:02:24 +0400

    feat:

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  var count=0;
+for (var i=0; i<backyard.length; i++){
+  var currentArr=backyard[i];
+  for (var j=0; j<currentArr.length; j++){
+  if (currentArr[j] == ('^^')){
+    count++;
+  }
+
+  }
+}
+console.log (count);
+return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(array) {
+  var output='';
+var count=0;
+var result;
+if (array.length===0){
+  result= false;
+  return result;
+}else {
+
+  for (var i=0; i<array.length; i++){
+      
+      if (typeof array[i] =='string'){
+        count ++;
+          output=output + array[i].charAt(0);
+      }
+
+      }
+     output=output.split('').sort(compareS).join('').toUpperCase() ;
+     return output;
+    }    
+}
+function compareS(a, b) {
+  if (a > b) {
+  return 1;
+  } else if (a === b) {
+  return 0;
+  } else {
+  return -1;
+  }
+    }
