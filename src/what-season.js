22d10d17502eff87bb5826ec0cecd3e5191744a5
tokanito <78441278+tokanito@users.noreply.github.com>
2021-03-10 22:29:50 +0400

    fix:

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+    var result;
+    
+    var str1= (date.toISOString());
+    str=str1.substr(5,2);
+    if (str<3){
+        result='winter';
+    }else if ((str<6)&&(str>=3)){
+        result = 'spring';
+    }else if ((str<9)&&(str>=6)){ 
+        result = 'summer';
+    }else if ((str<13)&&(str>=9)){ 
+        result = 'autumn';  
+    } 
+
+    return result;
+    
+}  
+
