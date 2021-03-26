7769aad7fd1567d0118aad20a6897a6b0f6d781f
Andrey Pleskunou <apleskunov@bk.ru>
2021-03-26 13:26:19 +0300

    fix: try to resolve problem

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-        Array.isArray(arr);
-        var newArr;
+    Array.isArray(arr);
+    let newArr;
         newArr = arr.map((element, index) => {
             if (arr[index] === undefined) {
                 arr.splice(index, 1);
             }
-            if (arr[index] === '--double-prev') {
+            if (arr[index] === '--double-prev' && index > 0) {
+                arr.splice(index, 1, arr[index - 1]);
+            } else if ( index === 0) {
                 arr.splice(index, 1);
+            }
+            if (arr[index] === '--double-next' && index + 1 < arr.length && index != 0){
+                arr.splice(index, 1, arr[index + 1]);
+            } else if ( index === arr.length -1) {
+                arr.splice(index, 1);
+            } else if ( index === 0) {
+                arr.splice(index, 1, arr[index], arr[index], );
+            }
 
-            }
-            if (arr[index + 1] === '--double-prev') {
-                arr[index + 1] = arr[index];
+            /* if (arr[index] === '--discard-prev' && index > 0 && index + 1 < arr.length) {
+                arr.splice(index, 2);
+            } else if (index === 0) {
+                arr.splice(index, 1);
+            } else if (index === arr.length - 1) {
+                arr.splice(index, 2);
+            } */
 
-            }
-            if (arr[index] === '--double-next') {
-                arr[index] = arr[index + 1];
-            }
-            if (arr[index + 1] === '--double-next') {
-                arr[index + 1] = arr[index + 2];
-            }
             if (arr[index] === '--discard-prev') {
                 arr.splice(index, 1);
 
@@ -42,9 +49,10 @@ module.exports = function transform(arr) {
             }
 
 
+
         });
         /* console.log(arr); */
         return arr;
 
     }
-    /* transform(["--discard-next", "DEF", true, true, "DEF"]); */
+    /* transform(['--discard-prev', "1", 2, '--discard-prev',  '3', 4, 'discard-prev']); */
