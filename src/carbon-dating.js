dc6404c3e49aea9a146cd59499f9c3cb5e57c87a
madinaakhrieva <akhrieva03@gmail.com>
2021-03-29 23:54:15 +0300

    feat: change code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
+  let i;
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
