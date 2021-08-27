febf4d9b4509a6e18814c2139a6cbf66974ee790
Maksim Shylau <shilaums@gmail.com>
2021-08-27 19:52:14 +0300

    fix: restore missing func name

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function (/* n */) {
+export default function deleteDigit(/* n */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
