92a1f1c62de3a81b9085220f319b2070684ca4e6
Maksim Shylau <shilaums@gmail.com>
2021-08-28 20:52:55 +0300

    feat: add skeleton of binary-search-tree

---
+++ src/binary-search-tree.js
@@ -0,0 +1,47 @@
+import { NotImplementedError } from '../extensions/index.js';
+
+/**
+ * Implement node for binary search tree
+ */
+class Node {}
+
+/**
+ * Implement simple binary search tree
+ */
+export default class BinarySearchTree {
+  root() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  add() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  find() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  has() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  remove() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  min() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+
+  max() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+  
+}
