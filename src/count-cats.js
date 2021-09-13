5112fe127589222d7b5a72a18e27322164b858a3
na1led <76558831+na1led@users.noreply.github.com>
2021-09-13 14:40:24 +0500

    Solution for Count Cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,6 +14,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
+function countCats(arr){
+    let count = 0
+    arr.forEach(el => {
+        for (let i = 0; i < el.length; i++) {
+            if (el[i] == '^^') {
+                count++
+            }
+        }
+    });
+    return count
+}
+
 export default function countCats(/* matrix */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
