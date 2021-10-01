cebd83e3ca09a81276a76df1f5f59d6d1f1e1777
tashasun <natalia.limbakh@gmail.com>
2021-10-01 11:28:35 +0300

    feat: carbon, cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+export default function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let Number = 0;
+  if(typeof sampleActivity!=="string") return false;
+  // if ( !sampleActivity || isNaN(sampleActivity) || sampleActivity <=0 || sampleActivity >15) {return false}
+  if (  isNaN(sampleActivity) || sampleActivity <=0 || sampleActivity >15) {return false}
+ 
+  {Number = Math.ceil(Math.log((15/sampleActivity))*5730/(693/1000))}
+// console.log(Number)
+// console.log(typeof(Number))
+// if ( Number <=0 || Number == Infinity) {return false;}
+// {
+return Number;
+// }
 }
+// console.log(dateSample("3"));
+
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,7 +14,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let Number =0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      console.log(`  item {${i}, ${j}} - ${matrix[i][j]}`);
+      if (matrix[i][j] == "^^") {Number=Number+1}
+    }
+  }
+return Number;
 }
+// countCats([
+//   [0, 1, '^^'],
+//    [0, '^^', 2],
+//   ['^^', 1, 2]
+//   ]) 
