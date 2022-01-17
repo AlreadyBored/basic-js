ed2ff5a127a9ce670149b84a3c703fbebc0a2419
Maksim Shylau <shilaums@gmail.com>
2022-01-17 23:06:23 +0100

    refactor: migrate mac address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-module.exports = function isMAC48Address(/* n */) {
+function isMAC48Address(/* n */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+module.exports = {
+  isMAC48Address
+};
