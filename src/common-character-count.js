825582c7f6fa701a7937e63b2d0e88ba74c1b341
Ali <mulwor.001@yandex.ru>
2022-04-27 14:45:54 +0300

    feat: mac-address

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount( s1, s2 ) {
+  //
 }
 
 module.exports = {
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address( n ) {
+  return /^([0-9a-f]{2}-){5}[0-9a-f]{2}$/i.test(n)
 }
 module.exports = {
   isMAC48Address
