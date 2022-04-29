637d1714bfb977e1fca0d5e10d23bde549a0a0ab
Vadzim Antonau <vanetthor@gmail.com>
2022-04-29 13:13:21 +0400

    refactor: reorder recursive-depth tests

--- test/recursive-depth.test.js
+++ test/recursive-depth.test.js
@@ -30,14 +30,14 @@ describe('Recursive depth', () => {
             }
         });
         it.optional('returns correct depth of nested arrays', () => {
-            assert.equal(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
-            assert.equal(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
-            assert.equal(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]), 3);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 5);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 15);
-            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 25);
             assert.equal(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 31);
+            assert.equal(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 5);
+            assert.equal(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]), 3);
+            assert.equal(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 25);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);
+            assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 15);
         });
         it.optional('works recursively', () => {
             const spy1 = sinon.spy(instance, 'calculateDepth');
