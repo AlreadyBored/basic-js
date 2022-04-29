4e6c794628a2b009233c70ec524784c4a5d9bfde
Ted McGregor <tegmcgregor@zohomail.com>
2022-04-29 04:22:59 +0300

    feat: carbon dating works

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string' || !+sampleActivity || +sampleActivity <= 0 || sampleActivity >= MODERN_ACTIVITY) return false;
+  return Math.ceil(HALF_LIFE_PERIOD * Math.log(+sampleActivity / MODERN_ACTIVITY) / - 0.693);
 }
 
 module.exports = {
