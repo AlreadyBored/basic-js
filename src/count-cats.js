079e06a1f4945e7e23ca30aa8e48a9b823510bc9
djjukio <42944001+djjukio@users.noreply.github.com>
2022-05-02 19:19:14 +0300

    feat: add old file

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,13 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-module.exports = {
-  countCats
+module.exports = function countCats(matrix) {
+  let result = 0;
+  
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === "^^") {
+        result++;
+      }
+    }
+  }
+  
+  return result;
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,24 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Calculate turns number and time (in seconds) required
- * to solve puzzle
- * 
- * @param {Number} disks number of disks
- * @param {Number} turnsSpeed speed (in turns/hour)
- * @return {Object} object with props turns (number of turns)
- * and seconds (time in seconds)
- *
- * @example
- * 
- * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
- *
- */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-module.exports = {
-  calculateHanoi
-};
+ module.exports = function calculateHanoi(diskNumber,turnsSpeed) {
+        let turns = Math.pow(2, diskNumber);
+        let seconds = turns / (turnsSpeed  / 3600);
+        return {turns: turns, seconds: seconds};
+        }
