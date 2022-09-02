5bf154c98a59b0e0a0aab65c36e5e9352a243b72
tkornilova <kornilova.tat07@gmail.com>
2022-09-02 22:04:29 +0400

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -14,10 +12,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
- */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+**/
+
+function repeater(str, options) {
+  let tempArr = [];
+  
+  if (typeof options.additionRepeatTimes === 'undefined') {
+    if (typeof options.addition !== 'undefined') {
+      tempArr.push(String(options.addition));
+    }
+  } else {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      tempArr.push(String(options.addition));
+    }
+  }
+  
+  if (typeof options.additionSeparator === 'undefined') {
+    tempArr = tempArr.join('|');
+  } else {
+    tempArr = tempArr.join(options.additionSeparator);
+  }
+  
+  str = `${String(str)}${tempArr}`;
+  tempArr = [];
+  
+  if (typeof options.repeatTimes === 'undefined') {
+    tempArr.push(str);
+  } else {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      tempArr.push(str);
+    }
+  }
+  
+  if (typeof options.separator === 'undefined') {
+    tempArr = tempArr.join('+');
+  } else {
+    tempArr = tempArr.join(options.separator);
+  }
+  
+  return tempArr;
 }
 
 module.exports = {
