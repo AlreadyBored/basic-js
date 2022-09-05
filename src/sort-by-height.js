3faa1838c2206d25d0be012e46ad6c06f2948b4b
tkornilova <kornilova.tat07@gmail.com>
2022-09-05 20:02:35 +0400

    refactor: formatting

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given an array with heights, sort them except if the value is -1.
  *
@@ -10,7 +8,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
+**/
+
 function sortByHeight(arr) {
   let result = [];
   let tempArr = [];
