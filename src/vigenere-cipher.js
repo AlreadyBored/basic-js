d65c91390970bf6c090b28044e59df7418497874
Nurlan <info.yourist@mail.ru>
2022-10-15 19:51:10 +0300

    feat:add last solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+ function isAlphabet(char) {
+  return char.toUpperCase() !== char.toLowerCase();
+}
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #CODE_A = 'A'.charCodeAt(0);
+
+  encrypt(message, key) {
+    if (!(message && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageArr = message.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('').map(v => v.charCodeAt(0) - this.#CODE_A);
+    let keyIndex = 0;
+    let res = [];
+
+    for (let i = 0; i < messageArr.length; i++) {
+      if (isAlphabet(messageArr[i])) {
+        res.push(String.fromCharCode(this.#CODE_A + (messageArr[i].charCodeAt(0) - this.#CODE_A + keyArr[keyIndex % keyArr.length]) % 26));
+        keyIndex++;
+      } else {
+        res.push(messageArr[i]);
+      }
+    }
+
+    return this.type ? res.join('') : res.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!(message && key)) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let messageArr = message.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('').map(v => v.charCodeAt(0) - this.#CODE_A);
+    let keyIndex = 0;
+    let res = [];
+
+    for (let i = 0; i < messageArr.length; i++) {
+      if (isAlphabet(messageArr[i])) {
+        res.push(String.fromCharCode(this.#CODE_A + (messageArr[i].charCodeAt(0) - this.#CODE_A - keyArr[keyIndex % keyArr.length] + 26) % 26));
+        keyIndex++;
+      } else {
+        res.push(messageArr[i]);
+      }
+    }
+
+    return this.type ? res.join('') : res.reverse().join('');
   }
 }
 
