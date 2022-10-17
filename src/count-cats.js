45786bdc57bdd42a27279b273ab4bc7ced0f6fca
Sergey <sergey.grinkevich@gmail.com>
2022-10-18 00:29:19 +0300

    feat: add function for task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(array) {
+  let sumCat = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] == '^^') {
+        sumCat++;
+      }
+    }
+  }
+  return sumCat;
 }
+
+
 
 module.exports = {
   countCats
