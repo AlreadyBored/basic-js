cd64b9e925120566598668dd05f24800995d0b32
George Bashirov <goshan397@gmail.com>
2022-10-18 18:28:45 +0300

    another basicJs tasks

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,10 +10,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let res = "";
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === "@" && email.charCodeAt(i + 1) !== 46) {
+      res += email.slice(i + 1);
+    }
+  }
+
+  return res;
 }
+
 
 module.exports = {
   getEmailDomain
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let arr = String(n).split('');
+  let result = 0;
+  let min = Math.min(...arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    result += +arr[i];
+    if (result >= 10) {
+      result = min;
+    }
+  }
+
+  return result;
+
 }
 
 module.exports = {
