2e0fe269dd727c666eda5eac9e3faca8ae129e9d
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-04-15 19:00:00 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,27 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || isNaN(Number(sampleActivity)) || Number(sampleActivity) <= 0 || Number(sampleActivity) > MODERN_ACTIVITY) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  return Math.ceil(t);
+}
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
 
 /**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix 
+ * @return {Number} count of cats found
  *
  * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
+ * countCats([
+ *  [0, 1, '^^'],
+ *  [0, '^^', 2],
+ *  ['^^', 1, 2]
+ * ]) => 3`
  *
  */
-function dateSample(/* sampleActivity */) {
+function countCats(/* matrix */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
 module.exports = {
-  dateSample
+  countCats
 };
