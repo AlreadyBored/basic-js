b1b49d6153bbac776408533f64c490ac723a18fd
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-04-17 15:37:29 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,25 +8,28 @@ function dateSample(sampleActivity) {
 }
 
 
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+
 /**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
+ * Determine the age of archeological find by using
+ * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
+ * 
+ * @param {String} sampleActivity string representation of current activity 
+ * @return {Number | Boolean} calculated age in years or false
+ * in case of incorrect sampleActivity
  *
  * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
+ * 
+ * dateSample('1') => 22387
+ * dateSample('WOOT!') => false
  *
  */
-function countCats(/* matrix */) {
+function dateSample(/* sampleActivity */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
 module.exports = {
-  countCats
+  dateSample
 };
