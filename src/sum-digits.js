ae9936b1aa4b2a8fdcd5a13f409feddebde1f97b
Bodnar Alex <allugovskova@mail.ru>
2023-12-14 04:44:08 +0200

    feat: solution 2 tasks

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,11 +12,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let res = toArray(n);
+  console.log(res);
+  while( res.length > 1){
+    res = res.reduce((acc, number) => acc + number);
+    if(res > 9){
+      res = toArray(res);
+    }
+  }
+  return res;
 }
 
 module.exports = {
   getSumOfDigits
 };
+
+
+function toArray(num){
+  return String(num).split("").map((x)=>Number(x));
+}
--- src/what-season.js
+++ src/what-season.js
@@ -2,14 +2,14 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 function getSeason(/* date */) {
   throw new NotImplementedError('Not implemented');
